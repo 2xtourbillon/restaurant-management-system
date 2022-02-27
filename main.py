1a9d49782cb9fc050b1215069214699cab35f769
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-26 22:51:20 -0500

    add reset and clear funcs

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from ast import Str
-from asyncore import file_dispatcher
-from pickletools import stringnl_noescape
 from tkinter import *
 import random
 import time
@@ -59,90 +56,141 @@ def equals():
     text_Input.set(sumup)
     operator = ""
 
+def Ref():
+    x = random.randint(12000, 50000)
+    randomRef = str(x)
+    rand.set(randomRef)
+
+    fries = float(Fries.get())
+    lunch_meal = float(LargeFries.get())
+    burger_meal = float(Burger.get())
+    pizza_meal = float(Filet.get())
+    cheese_burger = float(Cheese_Burger.get())
+    drinks = float(drinks.get())
+
+    fries_cost = fries*25
+    lunch_cost = lunch_meal*40
+    burger_cost = burger_meal*35
+    pizza_cost = pizza_meal*50
+    cheese_burger_cost = cheese_burger*30
+    drinks_cost = drinks*35
+
+    Meal_cost = 'Rs. ', str('%.2f'% (fries_cost + lunch_cost + burger_cost + pizza_cost + cheese_burger_cost + drinks_cost))
+    Total_cost = (fries_cost + lunch_cost + burger_cost + pizza_cost + cheese_burger_cost + drinks_cost)
+    Tax_Payable = ((fries_cost + lunch_cost + burger_cost + pizza_cost + cheese_burger_cost + drinks_cost)*0.33)
+    Ser_Charge = ((fries_cost + lunch_cost + burger_cost + pizza_cost + cheese_burger_cost + drinks_cost)/99)
+    Service = 'Rs. ', str('%.2f'% Ser_Charge)
+    OverallCost = 'Rs. ', str(Tax_Payable + Total_cost + Ser_Charge)
+    PaidTax = 'Rs. ', str('%.2f'% Tax_Payable)
+    Service_Charge.set(Ser_Charge)
+    Cost.set(Meal_cost)
+    Tax.set(PaidTax)
+    SubTotal.set(PaidTax)
+    Total.set(OverallCost)
+
+
+# exit function
+def qexit():
+    root.destroy()
+
+# reset function
+def reset():
+    rand.set('')
+    Fries.set('')
+    LargeFries.set('')
+    Burger.set('')
+    Filet.set('')
+    SubTotal.set('')
+    Total.set('')
+    Service_Charge.set('')
+    Drinks.set('')
+    Tax.set('')
+    Cost.set('')
+    Cheese_Burger.set('')
 
 # button #7
 btn7 = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='7', ng='black', command=lambda: btnclick(7))
+              text='7', bg='black', command=lambda: btnclick(7))
 btn7.grid(row=2, column=0)
 
 # button #8
 btn8 = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='8', ng='black', command=lambda: btnclick(8))
+              text='8', bg='black', command=lambda: btnclick(8))
 btn8.grid(row=2, column=1)
 
 # button #9
 btn9 = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='9', ng='black', command=lambda: btnclick(9))
+              text='9', bg='black', command=lambda: btnclick(9))
 btn9.grid(row=2, column=2)
 
 # additional
 Addition = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='+', ng='black', command=lambda: btnclick("+"))
+              text='+', bg='black', command=lambda: btnclick("+"))
 Addition.grid(row=2, column=3)
 
 # button #4
 btn4 = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='4', ng='black', command=lambda: btnclick(4))
+              text='4', bg='black', command=lambda: btnclick(4))
 btn4.grid(row=3, column=0)
 
 # button #5
 btn5 = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='5', ng='black', command=lambda: btnclick(5))
+              text='5', bg='black', command=lambda: btnclick(5))
 btn5.grid(row=3, column=1)
 
 # button #6
 btn6 = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='6', ng='black', command=lambda: btnclick(6))
+              text='6', bg='black', command=lambda: btnclick(6))
 btn6.grid(row=3, column=2)
 
 # Subtraction
 Subtraction = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='-', ng='black', command=lambda: btnclick("-"))
+              text='-', bg='black', command=lambda: btnclick("-"))
 Subtraction.grid(row=3, column=3)
 
 # button #1
 btn1 = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='1', ng='black', command=lambda: btnclick(1))
+              text='1', bg='black', command=lambda: btnclick(1))
 btn1.grid(row=4, column=0)
 
 # button #2
 btn2 = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='2', ng='black', command=lambda: btnclick(2))
+              text='2', bg='black', command=lambda: btnclick(2))
 btn2.grid(row=4, column=1)
 
 # button #3
 btn3 = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='3', ng='black', command=lambda: btnclick(3))
+              text='3', bg='black', command=lambda: btnclick(3))
 btn3.grid(row=4, column=2)
 
 # Multiply
 multiply = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='Multuply', ng='black', command=lambda: btnclick("*"))
+              text='Multuply', bg='black', command=lambda: btnclick("*"))
 multiply.grid(row=4, column=3)
 
 # button #0
 btn0 = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='0', ng='black', command=lambda: btnclick(0))
+              text='0', bg='black', command=lambda: btnclick(0))
 btn0.grid(row=5, column=0)
 
 # Clear
 btnc = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='c', ng='black', command=clrdisplay)
+              text='c', bg='black', command=clrdisplay)
 btnc.grid(row=5, column=1)
 
 # decimal
 decimal = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='.', ng='black', command=lambda: btnclick("."))
+              text='.', bg='black', command=lambda: btnclick("."))
 decimal.grid(row=5, column=2)
 
 # division
 division = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='/', ng='black', command=lambda: btnclick('/'))
+              text='/', bg='black', command=lambda: btnclick('/'))
 division.grid(row=5, column=3)
 
 # equal to
 btnequal = Button(f2, padx=16, pady=16, bd=4, fg='white', font=('arial', 20, 'bold'),
-              text='=', ng='black', command=equals)
+              text='=', bg='black', command=equals)
 btnequal.grid(columnspan=4)
 
 
@@ -242,7 +290,7 @@ btnReset = Button(f1, padx=16, pady=8, bd=10, fg='black', font=('Arial', 16, 'bo
 btnReset.grid(row=7, column=2)
 
 # Exit Button
-btnExit = Button(f1, padx=16, pady=8, bd=10, fg='black', font=('Arial', 16, 'bold'), width=10, text='EXIT', bg='powder blue', command=exit)
+btnExit = Button(f1, padx=16, pady=8, bd=10, fg='black', font=('Arial', 16, 'bold'), width=10, text='EXIT', bg='powder blue', command=qexit)
 btnExit.grid(row=7, column=3)
 
 def price():
